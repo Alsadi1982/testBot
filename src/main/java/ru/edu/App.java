d532cf8993f5164cabf68475d59fc2449c10c0a4
Alex Sadikov <sav1982@inbox.ru>
2022-04-29 00:11:46 +0300

    implemented simple Telgram Bot

--- src/main/java/ru/edu/App.java
+++ src/main/java/ru/edu/App.java
@@ -1,5 +1,9 @@
 package ru.edu;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new MyTestBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
